155369a54bd222081bc6c8ea667f2952cf79652c
Guilherme Gonçalves <gui.denez56@gmail.com>
2026-08-03 21:45:56 -0300

    Patch: This commit updates the securityFilterChain to authorize unauthenticated requests to /users -> POST.

--- src/main/java/br/com/foodapi/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/foodapi/infra/security/SecurityConfiguration.java
@@ -1,12 +1,32 @@
 package br.com.foodapi.infra.security;
 
+import br.com.foodapi.generated.api.UsersApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, UsersApi.PATH_CADASTRAR_USUARIO).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
