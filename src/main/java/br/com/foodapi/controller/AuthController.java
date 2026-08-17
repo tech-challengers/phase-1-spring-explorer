047f9c3c6cb2075340afac4c1bd3a281bd29d536
Guilherme Gonçalves <gui.denez56@gmail.com>
2026-08-17 20:06:48 -0300

    patch: add postman collections

--- src/main/java/br/com/foodapi/controller/AuthController.java
+++ src/main/java/br/com/foodapi/controller/AuthController.java
@@ -12,7 +12,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
 public class AuthController implements AuthenticationApi {
 
