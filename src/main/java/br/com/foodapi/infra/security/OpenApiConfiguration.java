1f0898b07d04931fceedf5a4fab3580d13a0fe89
Guilherme Gonçalves <gui.denez56@gmail.com>
2026-08-18 20:07:33 -0300

    Update Swagger and Postman collections

---
+++ src/main/java/br/com/foodapi/infra/security/OpenApiConfiguration.java
@@ -0,0 +1,15 @@
+package br.com.foodapi.infra.security;
+
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfiguration {
+}
